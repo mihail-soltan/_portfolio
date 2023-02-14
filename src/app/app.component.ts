4487a0f569a199f0521e0bd5cf65a9687f93ca7d
mihail-soltan <mihail.soltan@protonmail.ch>
2023-02-15 01:11:06 +0200

    fix overflow on mobile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
-
+import { SharedService } from './services/shared.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'portfolio2';
+  isHamburgerOpen: boolean = false;
+
+  constructor(private sharedService: SharedService) {
+    this.sharedService.isHamburgerOpen.subscribe((data) => {
+      this.isHamburgerOpen = this.sharedService.isHamburgerOpen.value;
+    });
+  }
 }
