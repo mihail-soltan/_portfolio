05e07db16769c3a19c751135674fc4ae5d9b3ea9
mihail-soltan <mihail.soltan@protonmail.ch>
2023-02-15 01:11:54 +0200

    add service, minor fixes

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,17 +1,18 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, Input, OnInit } from '@angular/core';
+import { SharedService } from 'src/app/services/shared.service';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent implements OnInit {
+  navItems: string[] = ['HOME', 'PORTFOLIO', 'CONTACT ME'];
 
-  navItems: string[] = ['HOME', 'PORTFOLIO', 'CONTACT ME']
-  
-  constructor() { }
+  constructor(private sharedService: SharedService) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  toggleMenu() {
+    this.sharedService.toggleMenu();
   }
-
 }
