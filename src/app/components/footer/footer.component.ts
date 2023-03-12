1a47456955334654b5f4775de99d1c9d891cfcab
mihail-soltan <mihail.soltan@protonmail.ch>
2023-03-12 21:51:29 +0200

    fix footer on load bug

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
+import { filter, pairwise } from 'rxjs/operators';
+
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
@@ -14,28 +16,48 @@ export class FooterComponent implements OnInit {
   ];
   isContactPage: boolean = false;
   componentLoaded: boolean = false;
+  prevUrl: string = '';
+  nextUrl: string = '';
   constructor(private router: Router) {}
 
   ngOnInit(): void {
-    this.router.events.subscribe((event) => {
-      if (event instanceof NavigationEnd) {
+    // this whole thing checks if the previous and next url are both project pages and if so, it doesn't animate the footer
+    // the contact conditional is to check whether we should render the section before the footer or not (which happens to be in the same component for some reason)
+    setTimeout(() => {
+      this.componentLoaded = true;
+    }, 1000);
+    this.router.events
+      .pipe(
+        filter((event) => event instanceof NavigationEnd),
+        pairwise()
+      )
+      .subscribe(([prev, next]: any) => {
+        this.prevUrl = prev.url;
+        this.nextUrl = next.url;
+        if (
+          this.prevUrl.includes('/portfolio/') &&
+          this.nextUrl.includes('/portfolio/')
+        ) {
+          this.componentLoaded = true;
+          return;
+        }
         this.componentLoaded = false;
         setTimeout(() => {
           this.componentLoaded = true;
         }, 1000);
-        if (event.url === '/contact') {
+
+        if (next.url === '/contact') {
           this.isContactPage = true;
         } else {
           this.isContactPage = false;
         }
-      }
-    });
+      });
   }
 
   onContactClick() {
     window.scrollTo({ top: 0, behavior: 'smooth' });
-    setTimeout(()=>{
+    setTimeout(() => {
       this.router.navigate(['/contact']);
-    }, 200)
+    }, 200);
   }
 }
